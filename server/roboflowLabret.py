509da04ee43c8ef02245708dbd296f47d648ddbb
Oowu <brianlim.zyuan@gmail.com>
2024-06-19 13:39:05 +0800

    face preprocessing

--- server/roboflowLabret.py
+++ server/roboflowLabret.py
@@ -7,5 +7,7 @@ load_dotenv()
 rf = Roboflow(api_key=os.getenv("ROBOFLOW_API_KEY"))
 workspaceId = os.getenv("ROBOFLOW_WORKSPACE_ID")
 faceProjectId = os.getenv("ROBOFLOW_FACE_PROJECT_ID")
+itemProjectId = os.getenv("ROBOFLOW_ITEM_PROJECT_ID")
 
 rfLabretFaceProject = rf.workspace(workspaceId).project(faceProjectId)
+rfLabretItemProject = rf.workspace(workspaceId).project(itemProjectId)
